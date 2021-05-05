f81659f45892951023fb6687e132a92a280be8e1
Jaiwanth-Reddy <82499928+Jaiwanth-Reddy@users.noreply.github.com>
2021-05-05 11:38:24 +0530

    Updated to give multiple choices

--- conventionally_strong_password.py
+++ conventionally_strong_password.py
@@ -11,27 +11,34 @@ digits = [i for i in s.digits]
 
 data = [lowercase,uppercase,symbols,digits]
 
-part1,part2 = '',''
+def get_conven_pass():
+    part1,part2 = '',''
 
-for i in range(length - 4):
-    a = r(0,3)
-    b = c(data[a])
-    part1 += b
+    for i in range(length - 4):
+        a = r(0,3)
+        b = c(data[a])
+        part1 += b
 
-for i in range(0,4):            # part two of string to ensure
-    b = c(data[i])              # each category has at least
-    part2 += b                  # one element in the password
+    for i in range(0,4):            # part two of string to ensure
+        b = c(data[i])              # each category has at least
+        part2 += b                  # one element in the password
 
-temp = list(part1)
+    temp = list(part1)
 
-while (part2 != ''):
-    t = part2[0]                # each character of part2 being
-    part2 = part2[1:]           # added into random positions
-    temp.insert(r(0,len(temp)), t)  # of part1
+    while (part2 != ''):
+        t = part2[0]                # each character of part2 being
+        part2 = part2[1:]           # added into random positions
+        temp.insert(r(0,len(temp)), t)  # of part1
 
-final = ''
+    final = ''
 
-for i in temp:
-    final += str(i)
+    for i in temp:
+        final += str(i)
 
-print(final) 
+    return(final)
+
+
+print('Here are your passwords. choose from the following.\n'.title())
+
+for i in range(0,4):
+    print(get_conven_pass())
